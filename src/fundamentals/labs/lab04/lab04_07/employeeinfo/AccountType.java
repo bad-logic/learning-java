2c88ffcd9d0e8091ab15a980ae1ea56f95936bb6
bad-logic <45297269+bad-logic@users.noreply.github.com>
2024-03-04 22:54:09 -0600

    lab04 part 07

---
+++ src/fundamentals/labs/lab04/lab04_07/employeeinfo/AccountType.java
@@ -0,0 +1,7 @@
+package fundamentals.labs.lab04.lab04_07.employeeinfo;
+
+public enum AccountType {
+	CHECKING, 
+	SAVINGS, 
+	RETIREMENT;
+}
---
+++ src/fundamentals/labs/lab04/lab04_07/employeeinfo/Employee.java
@@ -0,0 +1,104 @@
+package fundamentals.labs.lab04.lab04_07.employeeinfo;
+
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import fundamentals.labs.lab04.lab04_07.MyStringList;
+
+public class Employee implements Comparable<Employee> {
+	private AccountList accounts; 
+	private String name;
+	private Date hireDate;
+	
+	public Employee(String name, int year, int month, int day){
+		this.name = name;
+		GregorianCalendar cal = new GregorianCalendar(year, month, day);
+		hireDate = cal.getTime();
+		accounts =  new AccountList();
+	}
+
+	/**
+	 * Compares this object with the specified object for order.  Returns a
+	 * negative integer, zero, or a positive integer as this object is less
+	 * than, equal to, or greater than the specified object.
+	 *
+	 * <p>The implementor must ensure {@link Integer#signum
+	 * signum}{@code (x.compareTo(y)) == -signum(y.compareTo(x))} for
+	 * all {@code x} and {@code y}.  (This implies that {@code
+	 * x.compareTo(y)} must throw an exception if and only if {@code
+	 * y.compareTo(x)} throws an exception.)
+	 *
+	 * <p>The implementor must also ensure that the relation is transitive:
+	 * {@code (x.compareTo(y) > 0 && y.compareTo(z) > 0)} implies
+	 * {@code x.compareTo(z) > 0}.
+	 *
+	 * <p>Finally, the implementor must ensure that {@code
+	 * x.compareTo(y)==0} implies that {@code signum(x.compareTo(z))
+	 * == signum(y.compareTo(z))}, for all {@code z}.
+	 *
+	 * @param o the object to be compared.
+	 * @return a negative integer, zero, or a positive integer as this object
+	 * is less than, equal to, or greater than the specified object.
+	 * @throws NullPointerException if the specified object is null
+	 * @throws ClassCastException   if the specified object's type prevents it
+	 *                              from being compared to this object.
+	 * @apiNote It is strongly recommended, but <i>not</i> strictly required that
+	 * {@code (x.compareTo(y)==0) == (x.equals(y))}.  Generally speaking, any
+	 * class that implements the {@code Comparable} interface and violates
+	 * this condition should clearly indicate this fact.  The recommended
+	 * language is "Note: this class has a natural ordering that is
+	 * inconsistent with equals."
+	 */
+	@Override
+	public int compareTo(Employee o) {
+		double currentMax = this.accounts.getLargest().getBalance();
+		double oMax = o.accounts.getLargest().getBalance();
+		return Double.compare(currentMax, oMax);
+	}
+
+	/** returns a list of account types in the order in which 
+	 *  accounts are stored in the AccountList
+	 */
+	public MyStringList getNamesOfAccounts(){
+		MyStringList names = new MyStringList();
+		for(int i = 0; i < accounts.size(); ++i){
+			names.add(accounts.get(i).getAcctType().toString().toLowerCase());
+		}
+		return names;
+		
+	}
+	
+	public void createNewSavings(double startBalance){
+		accounts.add(new SavingsAccount(this,startBalance));
+	}
+	public void createNewChecking(double startBalance){
+		accounts.add(new CheckingAccount(this,startBalance));
+	}
+	public void createNewRetirement(double startBalance){
+		accounts.add(new RetirementAccount(this,startBalance));
+
+	}
+	public void deposit(int acctIndex, double amt){
+		Account selected = accounts.get(acctIndex);
+		selected.makeDeposit(amt);
+	}
+	public boolean withdraw(int acctIndex, double amt){
+		Account selected = accounts.get(acctIndex);
+		return selected.makeWithdrawal(amt);
+	}	
+	
+
+	public String getName() {
+		return name;
+	}
+	public Date getHireDate(){
+		Date returnVal = (Date)hireDate.clone();
+		return returnVal;
+	}
+	private String n = System.getProperty("line.separator");
+	
+	@Override
+	public String toString() {
+		return name + " : " + accounts.getLargest().getBalance();
+	}
+}
