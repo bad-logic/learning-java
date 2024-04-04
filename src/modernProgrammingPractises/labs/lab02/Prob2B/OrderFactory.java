6fcc6dcdd60ea766271530d39688b7fd60d17e77
bad-logic <mailme.roshanbhattarai@gmail.com>
2024-04-03 19:56:58 -0500

    lab 02 maintain one to many multiplicities

--- src/modernProgrammingPractises/labs/lab02/Prob2B/OrderFactory.java
+++ src/modernProgrammingPractises/labs/lab02/Prob2B/OrderFactory.java
@@ -1,7 +1,9 @@
 package modernProgrammingPractises.labs.lab02.Prob2B;
 
 public class OrderFactory {
-    public static Order createOrder(){
-        return new Order(1, 2024,2,24);
+    public static Order createOrder(int orderNum, double price, double quantity){
+        Order order =new Order(1, 2024,2,24);
+        order.addOrderLines(orderNum,price,quantity);
+        return order;
     }
 }
