50397706bbda85558f8eb02e60a22e6519adf0c8
bad-logic <mailme.roshanbhattarai@gmail.com>
2024-04-03 20:34:57 -0500

    update lab 03_01

---
+++ src/modernpractises/labs/lab03/lab03_01/soln/PersonWithJob.java
@@ -0,0 +1,38 @@
+package modernpractises.labs.lab03.lab03_01.soln;
+
+public class PersonWithJob {
+	
+	private double salary;
+	private Person person;
+	
+	public double getSalary() {
+		return salary;
+	}
+
+	PersonWithJob(String n, double s) {
+		this.person = new Person(n);
+		this.salary = s;
+	}
+
+	PersonWithJob(String n) {
+		this.person = new Person(n);
+	}
+	
+	@Override
+	public boolean equals(Object aPersonWithJob) {
+		if(aPersonWithJob == null) return false;
+		if(!(aPersonWithJob instanceof PersonWithJob)) return false;
+		PersonWithJob p = (PersonWithJob)aPersonWithJob;
+		return this.person.equals(p.person);
+	}
+
+	public static void main(String[] args) {
+		PersonWithJob p1 = new PersonWithJob("Joe", 30000);
+		PersonWithJob p2 = new PersonWithJob("Joe");
+		//As PersonsWithJobs, p1 should be equal to p2
+		System.out.println("p1.equals(p2)? " + p1.equals(p2));
+		System.out.println("p2.equals(p1)? " + p2.equals(p1));
+	}
+
+
+}
