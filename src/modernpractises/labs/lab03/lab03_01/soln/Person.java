d03f806abb1cf5184f8a6dce2570acd9512fe105
bad-logic <45297269+bad-logic@users.noreply.github.com>
2024-04-03 20:34:57 -0500

    update lab 03_01

---
+++ src/modernpractises/labs/lab03/lab03_01/soln/Person.java
@@ -0,0 +1,23 @@
+package modernpractises.labs.lab03.lab03_01.soln;
+
+
+public class Person {
+	private String name;
+	Person(String n) {
+		name = n;
+	}
+	public String getName() {
+		return name;
+	}
+	@Override
+	public boolean equals(Object aPerson) {
+		if(aPerson == null) return false;
+		if(!(aPerson instanceof Person)) return false;
+		Person p = (Person)aPerson;
+		return this.name.equals(p.name);
+	}
+	public static void main(String[] args) {
+		
+	}
+
+}
