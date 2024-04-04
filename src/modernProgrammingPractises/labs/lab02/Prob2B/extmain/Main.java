783ccfb657531bb810c87182abf7d99133410b04
bad-logic <45297269+bad-logic@users.noreply.github.com>
2024-04-03 19:56:58 -0500

    lab 02 maintain one to many multiplicities

--- src/modernProgrammingPractises/labs/lab02/Prob2B/extmain/Main.java
+++ src/modernProgrammingPractises/labs/lab02/Prob2B/extmain/Main.java
@@ -5,7 +5,7 @@ import modernProgrammingPractises.labs.lab02.Prob2B.OrderFactory;
 
 public class Main {
     public static void main(String[] args){
-        Order order = OrderFactory.createOrder();
+        Order order = OrderFactory.createOrder(1,33.98,78);
         order.addOrderLines(2, 22, 48);
         order.addOrderLines(4, 27, 49);
         order.addOrderLines(6, 30, 65);
