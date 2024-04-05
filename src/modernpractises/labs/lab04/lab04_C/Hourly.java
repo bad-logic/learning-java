28ff0cfb394c5f25a80d50c7a150b0b9217ed421
mlsagar <sagar.maharjan617571@outlook.com>
2024-04-04 21:24:57 -0500

    Add: changes in class diagram and lab 4 c in progress

---
+++ src/modernpractises/labs/lab04/lab04_C/Hourly.java
@@ -0,0 +1,5 @@
+package modernpractises.labs.lab04.lab04_C;
+
+public class Hourly {
+
+}
---
+++ src/modernpractises/labs/lab04/lab04_C/Main.java
@@ -0,0 +1,14 @@
+package modernpractises.labs.lab04.lab04_C;
+
+public class Main {
+    public static void main(String[] args) {
+            List<Order> list = new ArrayList();  
+            list.add(new Order("100",LocalDate.of(2023, 2, 1),200)); 
+            list.add(new Order("100",LocalDate.of(2023, 2, 10),100)); 
+            Commissioned cm = new Commissioned("123",0.8,500,list); 
+            Employee[] emp = { new Salaried("121",4000), new Hourly("122",15.67,20),cm}; 
+            for(Employee e :emp){ 
+             e.print(3,2023); 
+        }   
+    } 
+}
