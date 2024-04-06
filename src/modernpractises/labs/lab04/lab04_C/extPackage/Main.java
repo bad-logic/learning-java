8b5cb5b126f64d2940c421adac40d9c123be9016
bad-logic <mailme.roshanbhattarai@gmail.com>
2024-04-05 20:33:36 -0500

    update lab04_C update

--- src/modernpractises/labs/lab04/lab04_C/extPackage/Main.java
+++ src/modernpractises/labs/lab04/lab04_C/extPackage/Main.java
@@ -7,21 +7,22 @@ import java.util.List;
 import modernpractises.labs.lab04.lab04_C.Commissioned;
 import modernpractises.labs.lab04.lab04_C.Employee;
 import modernpractises.labs.lab04.lab04_C.EmployeeFactory;
-import modernpractises.labs.lab04.lab04_C.Hourly;
 import modernpractises.labs.lab04.lab04_C.Order;
-import modernpractises.labs.lab04.lab04_C.Salaried;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		List<Order> list = new ArrayList<>(); 
+		List<Order> list = new ArrayList<>();
+        list.add(EmployeeFactory.createOrder("100",LocalDate.of(2023, 1, 1),200));
         list.add(EmployeeFactory.createOrder("100",LocalDate.of(2023, 2, 1),200)); 
         list.add(EmployeeFactory.createOrder("100",LocalDate.of(2023, 2, 10),100)); 
-        list.add(EmployeeFactory.createOrder("100",LocalDate.of(2023, 3, 10),100)); 
+        list.add(EmployeeFactory.createOrder("100",LocalDate.of(2023, 3, 10),100));
+
         Commissioned cm = EmployeeFactory.createCommissioned("123",0.8,500,list); 
         Employee[] emp = { EmployeeFactory.createSalaried("121",4000), EmployeeFactory.createHourly("122",15.67,20),cm}; 
-        for(Employee e :emp){ 
+
+        for(Employee e :emp){
          e.print(3,2023); 
         }
 	}
