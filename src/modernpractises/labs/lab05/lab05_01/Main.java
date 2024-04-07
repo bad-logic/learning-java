eb5b147c9bce5a02ceeb76910bf07f1fb2cf5246
bad-logic <mailme.roshanbhattarai@gmail.com>
2024-04-07 15:53:25 -0500

    update lab05_05

---
+++ src/modernpractises/labs/lab05/lab05_01/Main.java
@@ -0,0 +1,9 @@
+package modernpractises.labs.lab05.lab05_01;
+
+public class Main {
+
+    public static void main(String[] args){
+        SingletonClass key1 = SingletonClass.getSingletonObject();
+        SingletonClass key2 = SingletonClass.getSingletonObject();
+    }
+}
