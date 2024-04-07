61118fc671cb46572e19407133d7830cb470427d
bad-logic <mailme.roshanbhattarai@gmail.com>
2024-04-07 15:53:47 -0500

    update lab05_02

---
+++ src/modernpractises/labs/lab05/lab05_02/vehicle/ElectricCar.java
@@ -0,0 +1,8 @@
+package modernpractises.labs.lab05.lab05_02.vehicle;
+
+public class ElectricCar implements Vehicle{
+
+    public void startEngine(){
+        System.out.println("Vehicle: ElectricCar");
+    }
+}
