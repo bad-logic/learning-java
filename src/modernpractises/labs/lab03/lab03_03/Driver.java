8486748e28455dd315a5cc314e78fc978f9ccfa2
bad-logic <mailme.roshanbhattarai@gmail.com>
2024-04-07 18:28:44 -0500

    lab03_03 update

--- src/modernpractises/labs/lab03/lab03_03/Driver.java
+++ src/modernpractises/labs/lab03/lab03_03/Driver.java
@@ -4,25 +4,17 @@ package modernpractises.labs.lab03.lab03_03;
 public class Driver {
 
 	public static void main(String[] args) {
-		Admin admin = new Admin(new Condo(new Address("500 N 2nd street", "Fairfield", "IA", "52556"), 3));
-
 		Address[] addresses = {
 				new Address("111 Main", "Fairfield", "IA", "52556"),
 				new Address("200 Forest Ave", "Fairfield", "IA", "52556"),
-			    new Address("10 N. 4th St.", "Fairfield", "IA", "52556")
+				new Address("10 N. 4th St.", "Fairfield", "IA", "52556")
 		};
-
-		Property[] properties = {
+		Property[] objects = {
 				new House(addresses[0], 1200.0),
 				new Condo(addresses[1], 2),
 				new Trailer(addresses[2])
 		};
-
-		for(Property property: properties) {
-			admin.addProperty(property);
-		}
-
-		double totalRent = admin.computeTotalRent();
+		double totalRent = Admin.computeTotalRent(objects);
 		System.out.println(totalRent);
 	}
 }
