a3406924cff9e1be51109a3cf50b0f93c312b788
bad-logic <mailme.roshanbhattarai@gmail.com>
2024-05-02 13:55:58 -0500

    labs: lab03 variable name typo fix

--- src/designPatterns/labs/lab03/P3Interest.java
+++ src/designPatterns/labs/lab03/P3Interest.java
@@ -3,7 +3,7 @@ package designPatterns.labs.lab03;
 public class P3Interest extends InterestPromotionDecorator{
 
     P3Interest(InterestStrategy interestStrategy){
-        this.interesetStrategy = interestStrategy;
+        this.interestStrategy = interestStrategy;
     }
 
     /**
@@ -13,7 +13,7 @@ public class P3Interest extends InterestPromotionDecorator{
     @Override
     public Account addInterest(Account acc) {
         // add interest based on checking or saving accounts
-        this.interesetStrategy.addInterest(acc);
+        this.interestStrategy.addInterest(acc);
 
         // add p3 promotional interest
         double balance = acc.getBalance();
