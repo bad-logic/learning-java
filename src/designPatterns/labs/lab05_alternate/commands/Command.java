c00967af95a5d74e0329f722ab1a1600c9b52240
bad-logic <mailme.roshanbhattarai@gmail.com>
2024-05-06 22:23:42 -0500

    labs: lab05 alternate solution

--- src/designPatterns/labs/lab05_alternate/commands/Command.java
+++ src/designPatterns/labs/lab05_alternate/commands/Command.java
@@ -1,9 +1,8 @@
 package designPatterns.labs.lab05_alternate.commands;
 
-import designPatterns.labs.lab05_alternate.Account;
 
 public interface Command {
-    abstract void execute(Account acc, double amount, String description);
-    abstract void undo(Account acc, double amount, String description);
-    abstract void redo(Account acc, double amount, String description);
+    abstract void execute();
+    abstract void redo();
+    abstract void undo();
 }
--- src/designPatterns/labs/lab05_alternate/commands/NoCommand.java
+++ src/designPatterns/labs/lab05_alternate/commands/NoCommand.java
@@ -1,32 +1,21 @@
 package designPatterns.labs.lab05_alternate.commands;
 
-import designPatterns.labs.lab05_alternate.Account;
-
 public class NoCommand implements Command {
-    /**
-     * @param acc
-     * @param amount
-     */
+
     @Override
-    public void execute(Account acc, double amount, String description) {
+    public void execute() {
 
     }
 
-    /**
-     * @param acc
-     * @param amount
-     */
+
     @Override
-    public void undo(Account acc, double amount, String description) {
+    public void undo() {
 
     }
 
-    /**
-     * @param acc
-     * @param amount
-     */
+
     @Override
-    public void redo(Account acc, double amount, String description) {
+    public void redo() {
 
     }
 }
