1fcccf4e7bcb5713874c1ac738fea9d1455f36c1
bad-logic <45297269+bad-logic@users.noreply.github.com>
2024-06-25 07:14:40 -0500

    chore: remove react folder

--- react/lab/src/common/interfaces/service.ts
+++
@@ -1,9 +0,0 @@
-import { Operation } from 'fast-json-patch';
-
-export interface Service<R, D, I> {
-  create(data: D): Promise<R>;
-  getAll(): Promise<R[]>;
-  get(id: I): Promise<R>;
-  delete(id: I): Promise<void>;
-  patch(id: I, data: Operation[]): Promise<void>;
-}
