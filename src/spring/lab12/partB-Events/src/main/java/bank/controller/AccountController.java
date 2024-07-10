4f7cf8a833d706ff5a6c439f79f4b5ad81bff8f7
bad-logic <45297269+bad-logic@users.noreply.github.com>
2024-07-09 22:29:36 -0500

    chore: lab 12 partB

---
+++ src/spring/lab12/partB-Events/src/main/java/bank/controller/AccountController.java
@@ -0,0 +1,43 @@
+package bank.controller;
+
+import bank.common.RestException;
+import bank.dtos.AccountDTO;
+import bank.dtos.CreateAccountDTO;
+import bank.dtos.TransactionDTO;
+import bank.service.AccountService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/accounts")
+public class AccountController {
+    @Autowired
+    private AccountService accountService;
+
+    @GetMapping
+    public ResponseEntity<List<AccountDTO>> getAccounts(){
+        return new ResponseEntity<List<AccountDTO>>(this.accountService.getAllAccounts(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<AccountDTO> addAccount(@RequestBody @Valid CreateAccountDTO data){
+        return new ResponseEntity<AccountDTO>(this.accountService.createAccount(data), HttpStatus.CREATED);
+    }
+
+    @PostMapping("/deposit")
+    public ResponseEntity<TransactionDTO> deposit(@RequestBody @Valid TransactionDTO data) throws RestException {
+        this.accountService.deposit(data);
+        return new ResponseEntity<TransactionDTO>(data, HttpStatus.OK);
+    }
+
+    @PostMapping("/withdraw")
+    public ResponseEntity<TransactionDTO> withdraw(@RequestBody @Valid TransactionDTO data) throws RestException {
+        this.accountService.withdraw(data);
+        return new ResponseEntity<TransactionDTO>(data, HttpStatus.OK);
+    }
+}
