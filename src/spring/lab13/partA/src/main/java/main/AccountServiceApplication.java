39c42b8df5f3bac58a8911c217b671c534857ade
bad-logic <45297269+bad-logic@users.noreply.github.com>
2024-07-11 07:21:08 -0500

    chore: lab13 partA

--- src/spring/lab13/partA/src/main/java/main/AccountServiceApplication.java
+++ src/spring/lab13/partA/src/main/java/main/AccountServiceApplication.java
@@ -1,14 +1,10 @@
-package accounts;
+package main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootApplication
-@EnableJpaRepositories("accounts.repositories")
-@EntityScan("accounts.domain") 
 public class AccountServiceApplication {
 
 	public static void main(String[] args) {
