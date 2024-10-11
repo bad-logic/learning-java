2ca36ad31d2d1ff5a2d48b44413a9738e63e0b9a
bad-logic <45297269+bad-logic@users.noreply.github.com>
2024-10-10 20:34:31 -0500

    algo: optimized

--- src/algorithms/labs/lab08/Permutations.java
+++ src/algorithms/labs/lab08/Permutations.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Permutations {
-    public static List<List<Integer>> permute(List<List<Integer>> result, List<Integer> permutation, int[] nums, int index){
-        if(index == nums.length){
+    public static List<List<Integer>> permute(List<List<Integer>> result, List<Integer> permutation, int[] nums){
+        if(permutation.size() == nums.length){
             result.add(new ArrayList<>(permutation));
             return result;
         }
@@ -13,17 +13,17 @@ public class Permutations {
         for(int n: nums){
             if(permutation.contains(n)) continue;
             permutation.add(n);
-            permute(result,permutation,nums,index+1);
+            permute(result,permutation,nums);
             permutation.removeLast();
         }
 
         return result;
     }
     public static List<List<Integer>> permute(int[] nums) {
-        return permute(new ArrayList<>(), new ArrayList<>(), nums,0);
+        return permute(new ArrayList<>(), new ArrayList<>(), nums);
     }
 
     public static void main(String[] args){
-        System.out.println(permute(new int[]{1,2,3}));
+        System.out.println(permute(new int[]{1,2,3,4}));
     }
 }
