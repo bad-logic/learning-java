3188fce39dd3345668c846a4e6957c697907d46d
bad-logic <45297269+bad-logic@users.noreply.github.com>
2025-06-17 18:50:08 -0500

    chore: add javadocs and package for templates module

--- src/springBoot/pdf-generator/templates/src/main/java/pdfgenerator/templates/PdfTemplate.java
+++ src/springBoot/pdf-generator/templates/src/main/java/pdfgenerator/templates/PdfTemplate.java
@@ -2,10 +2,40 @@ package pdfgenerator.templates;
 
 import java.util.Map;
 
+/**
+ * The interface Pdf template.
+ */
 public interface PdfTemplate {
+    /**
+     * Gets pdf data.
+     *
+     * @return the pdf data
+     */
     Map<String,Object> getPdfData();
+
+    /**
+     * Sets pdf data.
+     *
+     * @param data the data
+     */
     void setPdfData(Map<String,Object> data);
+
+    /**
+     * Validate boolean.
+     *
+     * @return the boolean
+     */
     boolean validate();
+
+    /**
+     * Generate.
+     */
     void generate();
+
+    /**
+     * Get pdf content byte [ ].
+     *
+     * @return the byte [ ]
+     */
     byte[] getPdfContent();
 }
--- src/springBoot/pdf-generator/templates/src/main/java/pdfgenerator/templates/PdfTemplateComponent.java
+++ src/springBoot/pdf-generator/templates/src/main/java/pdfgenerator/templates/PdfTemplateComponent.java
@@ -5,11 +5,25 @@ import java.lang.annotation.Target;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
+/**
+ * The interface Pdf template component.
+ */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface PdfTemplateComponent {
 
+	/**
+	 * Name string.
+	 *
+	 * @return the string
+	 */
 	String name();
+
+	/**
+	 * Singleton boolean.
+	 *
+	 * @return the boolean
+	 */
 	boolean singleton() default false;
 
 }
